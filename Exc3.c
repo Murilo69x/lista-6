6eea848953800f6b8a4f56946cd74dff9d00a706
Murilo69x <muriloolliveira8@gmail.com>
2026-05-13 19:45:46 -0300

    Create Exc3.c

---
+++ Exc3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+Int main() |
+Int numeros [10];
+ant c, mator = -9999;
+for(с = 0; с < 10; c++) ( printf("Digite o Número da Posição %d: ". C);
+scant ("%a", &numeros [c]);
+1f (numeros[c] > maior) (
+maior = numeros[c]:
+else(
+print (Mador Valor: Nd", maior);
+rotura 9;
+}
