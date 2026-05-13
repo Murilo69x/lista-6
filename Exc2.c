6a2045024b6735f01458fd4a085a07baff3fc12e
Murilo69x <muriloolliveira8@gmail.com>
2026-05-13 19:47:29 -0300

    Create Exc2.c

---
+++ Exc2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+Int numeros [8];
+int c, soma = 0;
+for (c = 0; c < 8; c++) C
+printf ("Digite o Número da Posição %d: ". c);
+}
+soma = soma + numeros [c];
+printf ("A Soma é: %d", soma) ;
+return 0;
+}
