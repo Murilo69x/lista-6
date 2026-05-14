320e26bc6a363950fae96f85a80fe20c15822ef3
Murilo69x <muriloolliveira8@gmail.com>
2026-05-13 21:02:06 -0300

    Create exc9.c

---
+++ exc9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int a[5], b[5], c[5];
+   int pos;
+   printf("alimentando vetor a");
+   for (pos = 0; pos < 5; pos++){
+       printf("digite o numero da posição %d", pos);
+       scanf("%d", &vetorb[pos]);
+       printf("alimentando vetor c");
+       for (pos = 0; pos < 5; pos++){
+           vetorc[pos] = vetora[pos] + vetorb[pos];       }
+   }
+printf("resultado vetor c");
+for (pos = 0; pos <5; pos ++){
+    printf ("%d\n", vetorc[pos);
+}
+    return 0;
